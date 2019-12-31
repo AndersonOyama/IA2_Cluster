54d8f39a08cd29cf7e93472e03eb5673c098028a
AndersonOyama <anderson.oyama@hotmail.com>
2019-12-31 01:26:27 -0300

    ?

--- svm_algoritmo.py
+++ svm_algoritmo.py
@@ -1,11 +1,15 @@
 import numpy as np
 import pandas as pd
+import sys
 from sklearn import svm
 
 
 def main():
 
-    data = pd.read_csv()
+    dataset = pd.read_csv("Dataset/wine.csv",";")
+    print(dataset)
+    dataset.shape
+    dataset.head()
 
 if __name__ == '__main__':
     main()
