81a898ba4ee171baae6648f268b56e6860accfd3
AndersonOyama <anderson.oyama@hotmail.com>
2020-01-18 19:33:11 -0300

    SVM

--- SVM/svm_algoritmo.py
+++ SVM/svm_algoritmo.py
@@ -5,38 +5,56 @@ import math
 import random
 
 from sklearn import svm
+from sklearn.model_selection import train_test_split
+from sklearn import metrics
+
 
 def main():
+    porcentagem = float(sys.argv[1])
+    entrada1 = pd.read_csv("../Dataset/winequality-red.csv", ";")
+    entrada2 = pd.read_csv("../Dataset/winequality-white.csv", ";")
+    treinoRed, treinoWhite, testeRed, testeWhite = gerador_treino(entrada1, entrada2, porcentagem)
 
-    treino_entrada1 = pd.read_csv("../Dataset/winequality-red.csv", ";")
-    treino_entrada2 = pd.read_csv("../Dataset/winequality-white.csv", ";")
-    # print(treino_entrada1.loc[1,:])
-    treinoRed, treinoWhite = gerador_treino(treino_entrada1, treino_entrada2, float(sys.argv[1]))
-    print(treinoRed)
-    categorias = ["red","white"]
+    X_train, X_test, y_train, y_test = train_test_split(entrada1, entrada1, test_size=0.25, random_state=100)
+
+    clf = svm.SVC(kernel='linear')
+    clf.fit(X_train, y_train)
+    y_pred = clf.predict(X_test)
+    print("Acuracia: ", metrics.accuracy_score(y_test, y_pred))
+    print(y_pred)
+    print(X_test)
+    print(X_train)
+
 
 
 def gerador_treino(wine1, wine2, porcentagem):
     baseRed = []
     baseWhite = []
-    i = 0
-    while (len(baseRed) < math.ceil(len(wine1)*porcentagem)):
-        if(i > len(wine1)):
-            i = 0
-        if ((randomBool()) == True):
-            baseRed.append(wine1.loc[(i), :])
-            i += 1
-        i += 1
-    i = 0
-    while (len(baseWhite) < math.ceil(len(wine2)*porcentagem)):
-        if(i>len(wine2)):
-            i=0
-        if ((randomBool()) == True):
-            baseWhite.append(wine2.loc[(i), :])
-            i += 1
-        i += 1
-    return (baseRed, baseWhite)
+    testeRed = []
+    testWhite = []
+    
+    sortIndex = randomIndex(len(wine1), math.ceil(len(wine1)*porcentagem))
+    for i in range(0, len(wine1)):
+        if (i in sortIndex):
+            baseRed.append(wine1.loc[(i),:])
+        else:
+            testeRed.append(wine1.loc[(i),:])
+    
+    sortIndex = randomIndex(len(wine2), math.ceil(len(wine2)*porcentagem))
+    for i in range(0, len(wine2)):
+        if (i in sortIndex):
+            baseWhite.append(wine2.loc[(i),:])
+        else:
+            testWhite.append(wine2.loc[(i),:])    
 
+    return (baseRed, baseWhite, testeRed, testWhite)
+
+
+def randomIndex(size, qtd):
+    index = random.sample(range(0,size), qtd)
+    index.sort()
+    return index
+    
 
 def randomBool():
     random_bit = random.getrandbits(1)
