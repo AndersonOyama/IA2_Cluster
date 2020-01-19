146a08c5851ef262873fb019aee11dbcfb7d24b3
AndersonOyama <anderson.oyama@hotmail.com>
2020-01-19 00:33:03 -0300

    SVM Finished. Miss plot

--- SVM/svm_algoritmo.py
+++ SVM/svm_algoritmo.py
@@ -7,59 +7,46 @@ import random
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 from sklearn import metrics
+from sklearn.utils import shuffle
+from sklearn.datasets.samples_generator import make_blobs
+
+import matplotlib.pyplot as plt
+import scikitplot as skplt
 
 
 def main():
     porcentagem = float(sys.argv[1])
-    entrada1 = pd.read_csv("../Dataset/winequality-red.csv", ";")
-    entrada2 = pd.read_csv("../Dataset/winequality-white.csv", ";")
-    treinoRed, treinoWhite, testeRed, testeWhite = gerador_treino(entrada1, entrada2, porcentagem)
+    redWine = pd.read_csv("../Dataset/winequality-red.csv", ";")
+    whiteWine = pd.read_csv("../Dataset/winequality-white.csv", ";")
 
-    X_train, X_test, y_train, y_test = train_test_split(entrada1, entrada1, test_size=0.25, random_state=100)
+    # treinoRed, treinoWhite, testeRed, testeWhite, answerRed, answerWhite,
+    treino, ans = gerador_treino(redWine, whiteWine)
 
-    clf = svm.SVC(kernel='linear')
+    X_train, X_test, y_train, y_test = train_test_split(treino, ans, test_size=porcentagem, random_state=0)
+
+    clf = svm.SVC(kernel='linear', gamma=2)
     clf.fit(X_train, y_train)
     y_pred = clf.predict(X_test)
     print("Acuracia: ", metrics.accuracy_score(y_test, y_pred))
-    print(y_pred)
-    print(X_test)
-    print(X_train)
+    print("Recall: ", metrics.recall_score(y_test, y_pred))
+
+    X, y = make_blobs(n_samples=( int(porcentagem * len(treino))), centers=2, random_state=0, cluster_std=0.60)
+    plt.scatter(X[:, 0], X[:, 1], c=y, s=50, cmap='autumn');
+    plt.show()
 
 
 
-def gerador_treino(wine1, wine2, porcentagem):
-    baseRed = []
-    baseWhite = []
-    testeRed = []
-    testWhite = []
-    
-    sortIndex = randomIndex(len(wine1), math.ceil(len(wine1)*porcentagem))
-    for i in range(0, len(wine1)):
-        if (i in sortIndex):
-            baseRed.append(wine1.loc[(i),:])
-        else:
-            testeRed.append(wine1.loc[(i),:])
-    
-    sortIndex = randomIndex(len(wine2), math.ceil(len(wine2)*porcentagem))
-    for i in range(0, len(wine2)):
-        if (i in sortIndex):
-            baseWhite.append(wine2.loc[(i),:])
-        else:
-            testWhite.append(wine2.loc[(i),:])    
-
-    return (baseRed, baseWhite, testeRed, testWhite)
+def gerador_treino(redWine, whiteWine):
+    baseTreino = []
+    baseClasse = []
+    ansRed = [0] * len(redWine)
+    ansWhite = [1] * len(whiteWine)
+    base = pd.concat([redWine, whiteWine], ignore_index=True)
+    classe = ansRed + ansWhite
+    baseTreino, baseClasse = shuffle(base, classe)
+    return baseTreino, baseClasse
 
 
-def randomIndex(size, qtd):
-    index = random.sample(range(0,size), qtd)
-    index.sort()
-    return index
-    
-
-def randomBool():
-    random_bit = random.getrandbits(1)
-    random_boolean = bool(random_bit)
-    return(random_boolean)
 
 if __name__ == '__main__':
     main()
